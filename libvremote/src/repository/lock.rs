a4305564c12c2c0999d816aa0ab4fd949a6b1dc9
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-10-30 12:02:22 -0500

    Reimplement add_volume() for Lock

--- libvremote/src/repository/lock.rs
+++ libvremote/src/repository/lock.rs
@@ -18,26 +18,7 @@ impl Lock {
     }
 
     pub fn add_volume(&mut self, volume: Volume) -> io::Result<()> {
-        // let staging_lock = path::PathBuf::from(STAGING_DIR).join("lock");
-        // let mut volumes: HashMap<String, Volume>
-        //     = match self.transport.get_file(&staging_lock) {
-        //         Ok(file) => serde_yaml::from_reader(file).unwrap(),
-        //         // If the file does not exist...
-        //         Err(e) => match e.kind() {
-        //             io::ErrorKind::NotFound => {
-        //                 // Attempt to copy it from DATA_DIR
-        //                 self.transport.copy(&LOCK_FILE, &staging_lock)?;
-        //                 serde_yaml::from_reader(
-        //                     self.transport.get_file(&staging_lock)?).unwrap()
-        //             },
-        //             _ => return Err(e),
-        //         },
-        //     };
-
-        // volumes.insert(volume.get_name().to_owned(), volume);
-        // let volumes = serde_yaml::to_string(&volumes).unwrap();
-        // self.transport.put_file(&staging_lock, &volumes.as_bytes())?;
-        // Ok(())
+        self.volumes.insert(volume.get_name().to_owned(), volume);
         unimplemented!()
     }
 }
