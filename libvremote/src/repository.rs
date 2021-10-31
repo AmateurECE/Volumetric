bb3a1981c78587c12a2a0664c54614ee829c4ec6
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-10-31 12:42:57 -0500

    Refactor RepositoryStructure and StageStructure

--- libvremote/src/repository.rs
+++ libvremote/src/repository.rs
@@ -7,7 +7,7 @@
 //
 // CREATED:         10/28/2021
 //
-// LAST EDITED:     10/30/2021
+// LAST EDITED:     10/31/2021
 //
 // Copyright 2021, Ethan D. Twardy
 //
@@ -25,6 +25,8 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ////
 
+use std::path::{Path, PathBuf};
+
 mod lock;
 mod object_store;
 mod settings;
@@ -37,32 +39,78 @@ pub use settings::DeploymentPolicy;
 pub use stage::Stage;
 
 const VOLUMETRIC_FILE: &'static str = "volumetric.yaml";
-const LOCK_FILE: &'static str       = ".volumetric/lock";
-const SETTINGS_FILE: &'static str   = ".volumetric/settings";
-const HISTORY_FILE: &'static str    = ".volumetric/history";
-const OBJECTS_DIR: &'static str     = ".volumetric/objects";
-const CHANGES_DIR: &'static str     = ".volumetric/changes";
+const DATA_DIR: &'static str        = ".volumetric";
+const LOCK_FILE: &'static str       = "lock";
+const SETTINGS_FILE: &'static str   = "settings";
+const HISTORY_FILE: &'static str    = "history";
+const OBJECTS_DIR: &'static str     = "objects";
+const CHANGES_DIR: &'static str     = "changes";
 
-const TMP_DIR: &'static str         = ".volumetric/staging/tmp";
-const STAGING_LOCK: &'static str    = ".volumetric/staging/lock";
-const STAGING_OBJECTS: &'static str = ".volumetric/staging/objects";
+const STAGE_DIR: &'static str       = "staging";
+const TMP_DIR: &'static str         = "tmp";
 
-pub struct StageStructure {}
-impl StageStructure {
-    pub fn get_tmp() -> &'static str { &TMP_DIR }
-    pub fn get_lock() -> &'static str { &STAGING_LOCK }
-    pub fn get_objects() -> &'static str { &STAGING_OBJECTS }
+pub struct StageStructure {
+    stage: PathBuf,
+    tmp: PathBuf,
+    lock: PathBuf,
+    objects: PathBuf,
 }
 
-pub struct RepositoryStructure {}
-impl RepositoryStructure {
-    pub fn get_volumetric() -> &'static str { &VOLUMETRIC_FILE }
-    pub fn get_lock() -> &'static str { &LOCK_FILE }
-    pub fn get_settings() -> &'static str { &SETTINGS_FILE }
-    pub fn get_history() -> &'static str { &HISTORY_FILE }
-    pub fn get_objects() -> &'static str { &OBJECTS_DIR }
-    pub fn get_changes() -> &'static str { &CHANGES_DIR }
-    pub fn get_stage() -> StageStructure { return StageStructure {} }
+impl<'a> StageStructure {
+    pub fn get_stage(&'a self) -> &'a Path { &self.stage }
+    pub fn get_tmp(&'a self) -> &'a Path { &self.tmp }
+    pub fn get_lock(&'a self) -> &'a Path { &self.lock }
+    pub fn get_objects(&'a self) -> &'a Path { &self.objects }
+}
+
+impl Default for StageStructure {
+    fn default() -> StageStructure {
+        let stage = PathBuf::from(DATA_DIR).join(STAGE_DIR);
+        StageStructure {
+            tmp: stage.to_owned().join(TMP_DIR),
+            lock: stage.to_owned().join(LOCK_FILE),
+            objects: stage.to_owned().join(OBJECTS_DIR),
+            stage,
+        }
+    }
+}
+
+pub struct RepositoryStructure {
+    volumetric: PathBuf,
+    data: PathBuf,
+    lock: PathBuf,
+    settings: PathBuf,
+    history: PathBuf,
+    objects: PathBuf,
+    changes: PathBuf,
+    stage: StageStructure,
+}
+
+impl<'a> RepositoryStructure {
+    pub fn get_volumetric(&'a self) -> &'a Path { &self.volumetric }
+    pub fn get_data(&'a self) -> &'a Path { &self.data }
+    pub fn get_lock(&'a self) -> &'a Path { &self.lock }
+    pub fn get_settings(&'a self) -> &'a Path { &self.settings }
+    pub fn get_history(&'a self) -> &'a Path { &self.history }
+    pub fn get_objects(&'a self) -> &'a Path { &self.objects }
+    pub fn get_changes(&'a self) -> &'a Path { &self.changes }
+    pub fn get_stage(&'a self) -> &'a StageStructure { &self.stage }
+}
+
+impl Default for RepositoryStructure {
+    fn default() -> RepositoryStructure {
+        let data = PathBuf::from(DATA_DIR);
+        RepositoryStructure {
+            volumetric: PathBuf::from(VOLUMETRIC_FILE),
+            lock: data.to_owned().join(LOCK_FILE),
+            settings: data.to_owned().join(SETTINGS_FILE),
+            history: data.to_owned().join(HISTORY_FILE),
+            objects: data.to_owned().join(OBJECTS_DIR),
+            changes: data.to_owned().join(CHANGES_DIR),
+            stage: StageStructure::default(),
+            data,
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
