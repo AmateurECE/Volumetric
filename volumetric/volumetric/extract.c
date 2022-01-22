2bcc979f9264af41cbcbca124d33db9f78196a12
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-01-22 15:16:51 -0600

    Implement basic file extraction interface (not integrated)

---
+++ volumetric/volumetric/extract.c
@@ -0,0 +1,115 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            extract.c
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     Implementation of the file extraction interface.
+//
+// CREATED:         01/22/2022
+//
+// LAST EDITED:     01/22/2022
+//
+// Copyright 2022, Ethan D. Twardy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////
+
+#include <stdlib.h>
+
+#include <archive.h>
+#include <archive_entry.h>
+
+#include <volumetric/extract.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Private API
+////
+
+static int copy_data(struct archive *ar, struct archive *aw) {
+    int r;
+    const void *buff;
+    size_t size;
+    la_int64_t offset;
+
+    for (;;) {
+        r = archive_read_data_block(ar, &buff, &size, &offset);
+        if (r == ARCHIVE_EOF)
+            return (ARCHIVE_OK);
+        if (r < ARCHIVE_OK)
+            return (r);
+        r = archive_write_data_block(aw, buff, size, offset);
+        if (r < ARCHIVE_OK) {
+            fprintf(stderr, "%s\n", archive_error_string(aw));
+            return (r);
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Public API
+////
+
+void extract_archive_to_disk_generic(const char *filename) {
+    struct archive *a;
+    struct archive *ext;
+    struct archive_entry *entry;
+    int flags;
+    int r;
+
+    /* Select which attributes we want to restore. */
+    flags = ARCHIVE_EXTRACT_TIME;
+    flags |= ARCHIVE_EXTRACT_PERM;
+    flags |= ARCHIVE_EXTRACT_ACL;
+    flags |= ARCHIVE_EXTRACT_FFLAGS;
+
+    a = archive_read_new();
+    archive_read_support_format_all(a);
+    archive_read_support_compression_all(a);
+    ext = archive_write_disk_new();
+    archive_write_disk_set_options(ext, flags);
+    archive_write_disk_set_standard_lookup(ext);
+    if ((r = archive_read_open_filename(a, filename, 10240)))
+        exit(1);
+    for (;;) {
+        r = archive_read_next_header(a, &entry);
+        if (r == ARCHIVE_EOF)
+            break;
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(a));
+        if (r < ARCHIVE_WARN)
+            exit(1);
+        r = archive_write_header(ext, entry);
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(ext));
+        else if (archive_entry_size(entry) > 0) {
+            r = copy_data(a, ext);
+            if (r < ARCHIVE_OK)
+                fprintf(stderr, "%s\n", archive_error_string(ext));
+            if (r < ARCHIVE_WARN)
+                exit(1);
+        }
+        r = archive_write_finish_entry(ext);
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(ext));
+        if (r < ARCHIVE_WARN)
+            exit(1);
+    }
+    archive_read_close(a);
+    archive_read_free(a);
+    archive_write_close(ext);
+    archive_write_free(ext);
+    exit(0);
+}
+
+///////////////////////////////////////////////////////////////////////////////
---
+++ volumetric/volumetric/extract.h
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            extract.h
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     An interface to construct files on disk from archives.
+//
+// CREATED:         01/22/2022
+//
+// LAST EDITED:     01/22/2022
+//
+// Copyright 2022, Ethan D. Twardy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////
+
+#ifndef VOLUMETRIC_EXTRACT_H
+#define VOLUMETRIC_EXTRACT_H
+
+void extract_archive_to_disk_generic(const char* filename);
+
+#endif // VOLUMETRIC_EXTRACT_H
+
+///////////////////////////////////////////////////////////////////////////////
