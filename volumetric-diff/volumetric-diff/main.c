222fc3fb5267707528d95a2ca44e138eb2bbf7fe
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-01-26 22:27:41 -0600

    (volumetric-diff) Locate the volume by name from the configuration

--- volumetric-diff/volumetric-diff/main.c
+++ volumetric-diff/volumetric-diff/main.c
@@ -26,9 +26,19 @@
 ////
 
 #include <argp.h>
-#include <config.h>
+#include <assert.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include <glib-2.0/glib.h>
+#include <gobiserde/yaml.h>
+
+#include <config.h>
 #include <volumetric/configuration.h>
+#include <volumetric/volume.h>
 
 const char* argp_program_version = "volumetric-diff " CONFIG_VERSION;
 const char* argp_program_bug_address = "<ethan.twardy@gmail.com>";
@@ -78,21 +88,101 @@ static error_t parse_opt(int key, char* arg, struct argp_state* state) {
     return 0;
 }
 
+struct VolumeFilter {
+    const char* name;
+    bool found;
+};
+
+static void compare_volume_name(void* key __attribute__((unused)), void* value,
+    void* user_data)
+{
+    struct VolumeFilter* filter = (struct VolumeFilter*)user_data;
+    Volume* volume = (Volume*)value;
+    if (!strcmp(filter->name, volume->archive.name)) {
+        filter->found = true;
+    }
+}
+
+static int find_volume_by_name(const char* path, const char* volume_name,
+    Volume* volume)
+{
+    struct dirent* entry = NULL;
+    DIR* directory = opendir(path);
+    if (NULL == directory) {
+        fprintf(stderr, "cannot open directory %s: %s\n", path,
+            strerror(errno));
+        return errno;
+    }
+
+    int result = 0;
+    size_t path_length = strlen(path);
+    char* whole_path = malloc(path_length + 256);
+    if (NULL == whole_path) {
+        fprintf(stderr, "Cannot allocate memory: %s\n", strerror(errno));
+    }
+
+    memset(whole_path, 0, path_length + 256);
+    strcat(whole_path, path);
+    whole_path[path_length] = '/';
+
+    while (NULL != (entry = readdir(directory))) {
+        // Ignore '.' and '..'
+        if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name)) {
+            continue;
+        }
+
+        size_t entry_length = strlen(entry->d_name);
+        memcpy(whole_path + path_length + 1, entry->d_name, entry_length);
+        whole_path[path_length + 1 + entry_length] = '\0';
+
+        FILE* input = fopen(whole_path, "rb");
+        if (NULL == input) {
+            fprintf(stderr, "error opening file %s: %s\n", entry->d_name,
+                strerror(errno));
+        }
+
+        VolumeFile volume_file = {0};
+        yaml_deserializer* yaml = gobiserde_yaml_deserializer_new_file(input);
+        result = volume_file_deserialize_from_yaml(yaml, &volume_file);
+        if (0 != result) {
+            break;
+        }
+
+        struct VolumeFilter filter = {0};
+        filter.name = volume_name;
+        g_hash_table_foreach(volume_file.volumes, compare_volume_name,
+            &filter);
+
+        gobiserde_yaml_deserializer_free(&yaml);
+        fclose(input);
+    }
+
+    free(whole_path);
+    closedir(directory);
+    return result;
+}
+
 static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 int main(int argc, char** argv) {
     struct arguments arguments = {0};
     arguments.configuration_file = CONFIGURATION_FILE;
+    int result = 0;
 
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
     VolumetricConfiguration config = {0};
-    int result = volumetric_configuration_load(arguments.configuration_file,
+    result = volumetric_configuration_load(arguments.configuration_file,
         &config);
-    if (0 != result) {
-        return result;
-    }
+    assert(result);
+
+    Volume volume = {0};
+    result = find_volume_by_name(config.volume_directory,
+        arguments.volume_name, &volume);
+    assert(result);
+
+    printf("volume.url=%s\n", volume.archive.url);
 
     volumetric_configuration_release(&config);
-    return 0;
+    return result;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
