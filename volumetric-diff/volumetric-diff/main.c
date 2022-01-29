78431bc4e545b84f2b5475e8d445e6c145f23978
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-01-29 09:12:38 -0600

    (volumetric-diff) Properly conditions the list of files now

--- volumetric-diff/volumetric-diff/main.c
+++ volumetric-diff/volumetric-diff/main.c
@@ -174,13 +174,19 @@ static int find_volume_by_name(const char* path, const char* volume_name,
     return result;
 }
 
-static int diff_file_lists(GPtrArray left, GPtrArray right) {
-    // TODO
+static int diff_file_lists(GPtrArray* left, GPtrArray* right) {
+    for (guint i = 0; i < left->len && NULL != left->pdata[i]; ++i) {
+        printf("left: %s\n", (const char*)left->pdata[i]);
+    }
+
+    for (guint i = 0; i < right->len && NULL != right->pdata[i]; ++i) {
+        printf("right: %s\n", (const char*)right->pdata[i]);
+    }
     return 0;
 }
 
-static GPtrArray get_file_list_for_archive(const char* archive_path) {
-    GPtrArray list = {0};
+static GPtrArray* get_file_list_for_archive(const char* archive_path) {
+    GPtrArray* list = g_ptr_array_new_with_free_func(free);
     struct archive *reader = archive_read_new();
     struct archive_entry *entry = NULL;
     archive_read_support_filter_all(reader);
@@ -190,7 +196,7 @@ static GPtrArray get_file_list_for_archive(const char* archive_path) {
     file_contents_init(&archive, archive_path);
     archive_read_open_memory(reader, archive.contents, archive.size);
     while (archive_read_next_header(reader, &entry) == ARCHIVE_OK) {
-        printf("%s\n",archive_entry_pathname(entry));
+        g_ptr_array_add(list, (gpointer)strdup(archive_entry_pathname(entry)));
     }
 
     archive_read_free(reader);
@@ -198,9 +204,9 @@ static GPtrArray get_file_list_for_archive(const char* archive_path) {
     return list;
 }
 
-static GPtrArray get_file_list_for_directory(const char* directory) {
-    GPtrArray list = {0};
-    // TODO: How does this work with symlinks? I'd expect they would break.
+static GPtrArray* get_file_list_for_directory(const char* directory) {
+    GPtrArray* list = g_ptr_array_new_with_free_func(free);
+
     char* directory_owned = malloc(strlen(directory) + 1);
     assert(NULL != directory_owned);
     strcpy(directory_owned, directory);
@@ -208,19 +214,48 @@ static GPtrArray get_file_list_for_directory(const char* directory) {
     FTS* tree = fts_open(paths, FTS_NOCHDIR, 0);
     assert(NULL != tree);
 
+    // TODO: How does this work with symlinks? I'd expect they would break.
     FTSENT* node = NULL;
     while ((node = fts_read(tree))) {
         if (FTS_F == node->fts_info || FTS_D == node->fts_info) {
-            printf("tree: %s\n", node->fts_path);
+            g_ptr_array_add(list, strdup(node->fts_path));
         } else if (FTS_ERR == node->fts_info || FTS_DNR == node->fts_info
             || FTS_NS == node->fts_info) {
-            printf("tree_error: %s\n", strerror(node->fts_errno));
+            fprintf(stderr, "fts_read error: %s\n", strerror(node->fts_errno));
         }
     }
 
     fts_close(tree);
     free(directory_owned);
     return list;
+}
+
+static void remove_matching_entry(GPtrArray* list, const char* match) {
+    // First, remove the entry corresponding to the match
+    guint index = 0;
+    for (guint i = 0; i < list->len && NULL != list->pdata[i]; ++i) {
+        if (!strcmp(match, list->pdata[i])) {
+            index = i;
+            break;
+        }
+    }
+    g_ptr_array_remove_index_fast(list, index);
+}
+
+static void trim_prefix_from_entries(GPtrArray* list, const char* prefix) {
+    // Then, remove the prefix from any entries that have it
+    size_t prefix_length = strlen(prefix);
+    for (guint i = 0; i < list->len && NULL != list->pdata[i]; ++i) {
+        if (!strncmp(prefix, list->pdata[i], prefix_length)) {
+            size_t string_length = strlen(list->pdata[i]);
+            char* string = malloc(string_length - prefix_length + 1);
+            assert(NULL != string);
+            strcpy(string, list->pdata[i] + prefix_length);
+            string[string_length] = '\0';
+            free(list->pdata[i]);
+            list->pdata[i] = string;
+        }
+    }
 }
 
 static int diff_volume(const char* volume_directory, const char* volume_name) {
@@ -234,8 +269,28 @@ static int diff_volume(const char* volume_directory, const char* volume_name) {
     docker_proxy_free(docker);
     assert(NULL != live_volume);
 
-    GPtrArray archive = get_file_list_for_archive(volume.archive.url);
-    GPtrArray directory = get_file_list_for_directory(live_volume->mountpoint);
+    GPtrArray* archive = get_file_list_for_archive(volume.archive.url);
+    GPtrArray* directory = get_file_list_for_directory(
+        live_volume->mountpoint);
+
+    // First, let's remove the top-level entry (either "./" or "/...")
+    remove_matching_entry(archive, "./");
+    trim_prefix_from_entries(archive, "./");
+
+    remove_matching_entry(directory, live_volume->mountpoint);
+    size_t mountpoint_length = strlen(live_volume->mountpoint);
+    if ('/' != live_volume->mountpoint[mountpoint_length - 1]) {
+        // Have to add that terminating '/'
+        char* mountpoint = malloc(mountpoint_length + 2);
+        strcpy(mountpoint, live_volume->mountpoint);
+        mountpoint[mountpoint_length] = '/';
+        mountpoint[mountpoint_length + 1] = '\0';
+        trim_prefix_from_entries(directory, mountpoint);
+        free(mountpoint);
+    } else {
+        trim_prefix_from_entries(directory, live_volume->mountpoint);
+    }
+
     result = diff_file_lists(archive, directory);
     return result;
 }
